42920786a54bef6e88356fc6d6e9f872731370ff
MsWakesho <wakeshosonja@gmail.com>
2023-12-22 11:47:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+const superbowRecords = [
+    {year: 2018, result: "W" },
+    {year: 2019, result: "L"},
+    {year: 2020, result: "L"},
+    {year:2021, result:"W"},
+]
+//Above is the Array of objects 
+
+
+
+
+function superbowlWin(resultsArray){ 
+ let winnningResult = resultsArray.find(result => result.result == "W");
+
+ return winnningResult ? winnningResult.year : undefined ;
+
+}
+
+let winningYear = superbowlWin(superbowlResults)
+console.log(winningYear);
+
